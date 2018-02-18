e367a64ae7f053a0bea0e804fdf1c368d57c0341
Frankie Sardo <fran.sardo@gmail.com>
2018-02-18 22:46:56 +0000

    Add datalog example

---
+++ example/test/example/datalog.clj
@@ -0,0 +1,14 @@
+(ns example.datalog
+  (:require [clojure.test :refer :all]
+            [moviola.core :as m]
+            [datascript.core :as d]))
+
+(defn seed-data [conn]
+  (d/transact! conn (for [i (range 10)]
+                      {:name (str "Customer-" i)
+                       :age (* 2 i)})))
+
+(deftest datalog-test
+  (let [conn (d/create-conn)]
+    (seed-data conn)
+    (m/match-snapshot ::datalog (d/q '[:find  ?n ?a :where [?e :name ?n] [?e :age ?a]] @conn))))
